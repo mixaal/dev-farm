5667d919a78d4f6bb695ab4cb1cf3d31f1e26543
Michal Conos <michal.conos@oracle.com>
2024-04-28 21:19:53 +0200

    use bare option to git init

--- git-server/git-web/src/git.rs
+++ git-server/git-web/src/git.rs
@@ -4,7 +4,6 @@ use std::{
     process::{Command, ExitStatus},
 };
 
-
 use crate::rest::{CreateRepoRequest, CreateRepoResponse};
 
 const REPO_PATH: &str = "/repo";
@@ -13,13 +12,13 @@ const CHOWN_CMD: &str = "/usr/bin/chown";
 const GIT_USER: &str = "git";
 const GIT_GROUP: &str = "git";
 
-const BARE_REPO: &str = "[core]
-	repositoryformatversion = 0
-	filemode = true
-	bare = true
-	logallrefupdates = true
+// const BARE_REPO: &str = "[core]
+// 	repositoryformatversion = 0
+// 	filemode = true
+// 	bare = true
+// 	logallrefupdates = true
 
-";
+// ";
 
 pub fn create_repo(req: CreateRepoRequest) -> std::io::Result<CreateRepoResponse> {
     let name = req.name;
@@ -28,7 +27,7 @@ pub fn create_repo(req: CreateRepoRequest) -> std::io::Result<CreateRepoResponse
     let method_name = "create_repo";
     tracing::info!(name, dir, method_name);
     fs::create_dir(&dir)?;
-    let r = git_cmd("init", &dir)?;
+    let r = git_init(true, &dir)?;
     if !r.success() {
         tracing::info!(method_name, dir, "git init failure");
         return Err(std::io::Error::new(
@@ -37,15 +36,17 @@ pub fn create_repo(req: CreateRepoRequest) -> std::io::Result<CreateRepoResponse
         ));
     }
 
-   
-
-    let config = format!("{dir}/.git/config");
-    let mut file = OpenOptions::new().write(true).truncate(true).create(true).open(config)?;
-    file.write_all(BARE_REPO.as_bytes())?;
+    // let config = format!("{dir}/.git/config");
+    // let mut file = OpenOptions::new()
+    //     .write(true)
+    //     .truncate(true)
+    //     .create(true)
+    //     .open(config)?;
+    // file.write_all(BARE_REPO.as_bytes())?;
 
     tracing::info!(method_name, dir, "configured as bare repository");
 
-    let r = chown_cmd(GIT_USER, GIT_GROUP,&dir)?;
+    let r = chown_cmd(GIT_USER, GIT_GROUP, &dir)?;
     if !r.success() {
         tracing::info!(method_name, dir, "chown failure");
         return Err(std::io::Error::new(
@@ -56,15 +57,30 @@ pub fn create_repo(req: CreateRepoRequest) -> std::io::Result<CreateRepoResponse
 
     tracing::info!(method_name, dir, "chown command executed");
 
-
     tracing::info!(method_name, dir, "repo created");
-    Ok(CreateRepoResponse { name: name.clone(), kind, commands: vec![format!("git clone ssh://git@localhost:7999/repo/{}", name)] })
+    Ok(CreateRepoResponse {
+        name: name.clone(),
+        kind,
+        commands: vec![format!("git clone ssh://git@localhost:7999/repo/{}", name)],
+    })
 }
 
-fn git_cmd(what: &str, cwd: &str) -> std::io::Result<ExitStatus> {
-    Command::new(GIT_CMD).arg(what).current_dir(cwd).status()
+fn git_init(bare: bool, cwd: &str) -> std::io::Result<ExitStatus> {
+    if bare {
+        Command::new(GIT_CMD)
+            .arg("init")
+            .arg("--bare")
+            .current_dir(cwd)
+            .status()
+    } else {
+        Command::new(GIT_CMD).arg("init").current_dir(cwd).status()
+    }
 }
 
 fn chown_cmd(user: &str, group: &str, dir: &str) -> std::io::Result<ExitStatus> {
-    Command::new(CHOWN_CMD).arg("-R").arg(format!("{}:{}", user, group)).arg(dir).status()
+    Command::new(CHOWN_CMD)
+        .arg("-R")
+        .arg(format!("{}:{}", user, group))
+        .arg(dir)
+        .status()
 }
